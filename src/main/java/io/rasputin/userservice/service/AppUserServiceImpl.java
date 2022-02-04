07dc3e8a455e616ae9cbd9b1893f9345181125dc
ThamerAyachi <thamerjak3@gmail.com>
2022-02-04 15:07:54 +0100

    Spring Security

--- src/main/java/io/rasputin/userservice/service/AppUserServiceImpl.java
+++ src/main/java/io/rasputin/userservice/service/AppUserServiceImpl.java
@@ -6,16 +6,37 @@ import io.rasputin.userservice.repo.AppUserRepo;
 import io.rasputin.userservice.repo.RoleRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class AppUserServiceImpl implements AppUserService{
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
 
     private final AppUserRepo appUserRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = appUserRepo.findByUsername(username);
+        if(user == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        }else {log.info("User found in the database {}", username);}
+        Collection<SimpleGrantedAuthority> authority = new ArrayList<>();
+        user.getRole().forEach(role -> {
+            authority.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new User(user.getUsername(), user.getPassword(), authority);
+    }
 
     @Override
     public AppUser saveUser(AppUser appUser) {
@@ -49,4 +70,5 @@ public class AppUserServiceImpl implements AppUserService{
         log.info("Fetching all users");
         return appUserRepo.findAll();
     }
+
 }
