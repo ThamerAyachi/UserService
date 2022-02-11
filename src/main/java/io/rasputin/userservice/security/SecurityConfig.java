11b4bd395270e183e7d61ade7f59c078821fc626
ThamerAyachi <thamerjak3@gmail.com>
2022-02-11 22:43:46 +0100

    jwt 3

--- src/main/java/io/rasputin/userservice/security/SecurityConfig.java
+++ src/main/java/io/rasputin/userservice/security/SecurityConfig.java
@@ -31,13 +31,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /** If I want to change Login path **/
+        /*
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        */
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().antMatchers("/login").permitAll();
+        /** If I want to change Login path **/
+        // http.authorizeRequests().antMatchers("/api/login/**").permitAll();
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        /** If I want to change Login path **/
+        // http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
