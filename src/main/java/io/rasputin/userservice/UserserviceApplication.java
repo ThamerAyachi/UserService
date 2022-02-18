1beb31cb3f8f6b62373424ef98d1e1fac6889c70
Thamer Ayachi <75398012+ThamerAyachi@users.noreply.github.com>
2022-02-18 16:43:40 +0100

    add CrossOrigin

--- src/main/java/io/rasputin/userservice/UserserviceApplication.java
+++ src/main/java/io/rasputin/userservice/UserserviceApplication.java
@@ -9,10 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
+@CrossOrigin(origins="*")
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
